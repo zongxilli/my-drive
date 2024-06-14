4085024dfe165824cfefdbe980c6a06b6a4a63ea
zongxi2014 <zongxi2014@gmail.com>
2024-06-13 22:50:41 -0400

    if org id is null, use userId instead

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,23 @@
 
 import { Button } from '@/components/ui/button';
 import { SignOutButton, SignedIn, SignedOut } from '@clerk/clerk-react';
-import { SignInButton, useOrganization } from '@clerk/nextjs';
+import { SignInButton, useOrganization, useUser } from '@clerk/nextjs';
 import { useMutation, useQuery } from 'convex/react';
 import { api } from '../../convex/_generated/api';
 
 export default function Home() {
-	const { organization } = useOrganization();
-	const orgId = organization?.id;
+	const organization = useOrganization();
+	const user = useUser();
+
+	let orgId = null;
+	if (organization.isLoaded && user.isLoaded) {
+		orgId = organization.organization?.id ?? user.user?.id;
+	}
 
 	const createFile = useMutation(api.files.createFile);
-	const files = useQuery(api.files.getFiles, { orgId: orgId ?? 'skip' });
+	const files = useQuery(api.files.getFiles, {
+		orgId: orgId ?? 'skip',
+	});
 
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
