4a022b0398fc043cf43e08ae4546f1e0107e7681
zongxi2014 <zongxi2014@gmail.com>
2024-06-14 19:36:41 -0400

    add ESLint rules and related packages and fix lint

--- src/app/ConvexClientProvider.tsx
+++ src/app/ConvexClientProvider.tsx
@@ -1,23 +1,23 @@
 'use client';
-import { ReactNode } from 'react';
-import { ConvexProvider, ConvexReactClient } from 'convex/react';
 import { ClerkProvider, useAuth } from '@clerk/nextjs';
-import { ConvexProviderWithClerk } from 'convex/react-clerk';
+import { ConvexProvider, ConvexReactClient } from 'convex/react';
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ReactNode } from "react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export default function ConvexClientProvider({
-	children,
+  children,
 }: {
-	children: ReactNode;
+  children: ReactNode;
 }) {
-	return (
-		<ClerkProvider
-			publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-		>
-			<ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-				{children}
-			</ConvexProviderWithClerk>
-		</ClerkProvider>
-	);
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 }
--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,29 +1,30 @@
-import { Button } from '@/components/ui/button';
 import {
-	OrganizationSwitcher,
-	SignInButton,
-	SignedOut,
-	UserButton,
-} from '@clerk/nextjs';
-import React from 'react';
+  OrganizationSwitcher,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
+import React from "react";
+
+import { Button } from "@/components/ui/button";
 
 const Header = () => {
-	return (
-		<div className='border-b py-4 bg-grey-50'>
-			<div className='container mx-auto flex justify-between items-center'>
-				<div>My Drive</div>
-				<div className='flex gap-2'>
-					<OrganizationSwitcher />
-					<UserButton />
-					<SignedOut>
-						<SignInButton mode='modal'>
-							<Button>Sign In</Button>
-						</SignInButton>
-					</SignedOut>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className='border-b py-4 bg-grey-50'>
+      <div className='container mx-auto flex justify-between items-center'>
+        <div>My Drive</div>
+        <div className='flex gap-2'>
+          <OrganizationSwitcher />
+          <UserButton />
+          <SignedOut>
+            <SignInButton mode='modal'>
+              <Button>Sign In</Button>
+            </SignInButton>
+          </SignedOut>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
